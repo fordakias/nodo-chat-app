8bdae14dbf7e6a310c463de47a19ceb609131092
fordakias <alexanderdedos@gmail.com>
2019-01-08 13:55:11 +0200

    Greet new user, and alerm others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New user connected');
 
+  socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user joined',
+    createAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message)=>{
     console.log('createMessage', message);
     io.emit('newMessage',{
