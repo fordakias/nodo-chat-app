d63fa6482939d83c0185e5b84c4d851d77d2c63e
fordakias <alexanderdedos@gmail.com>
2019-01-08 13:30:15 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
 
-  socket.emit('createMessage',{
-    from: 'Alex',
-    text: 'Yup, that work for me'
-  });
 });
 
 socket.on('disconnect', function() {
